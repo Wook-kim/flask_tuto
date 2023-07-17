58f5c3881cb223d4e06876d80c18124827e26b52
Wook Kim <decom0405@gmail.com>
2023-07-17 22:17:39 +0900

    2nd_0717

--- pybo/models.py
+++ pybo/models.py
@@ -11,4 +11,4 @@ class Answer(db.Model):
     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
     question = db.relationship('Question', backref=db.backref('answer_set',))
     content = db.Column(db.Text(), nullable=False)
-    create_db = db.Column(db.DateTime(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
