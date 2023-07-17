3f3630bba14aa4ff54c47a85ab4050060d96c044
Wook Kim <decom0405@gmail.com>
2023-07-17 21:56:18 +0900

    2nd_0717

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -8,4 +9,4 @@ def hello_pyob():
 
 @bp.route('/')
 def index():
-    return 'Index'
+    return redirect(url_for('question._list'))
